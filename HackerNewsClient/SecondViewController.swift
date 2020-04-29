147d76b254c7767655b2e650af9548dcab474831
Dinar Mukhametshin <dinar.mukhametshin@gmail.com>
2020-04-29 15:12:05 +0300

    Init a repository

---
+++ HackerNewsClient/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  HackerNewsClient
+//
+//  Created by Dinar Mukhametshin on 29.04.2020.
+//  Copyright © 2020 Dinar Mukhametshin. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
