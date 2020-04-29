ecae44bb6f7e4cb4ecee5cd4dfe4df3af6944d6b
Dinar Mukhametshin <dinar.mukhametshin@gmail.com>
2020-04-29 15:47:17 +0300

    Added new resources.

---
+++ HackerNewsClient/Color.swift
@@ -0,0 +1,9 @@
+//
+//  Color.swift
+//  HackerNewsClient
+//
+//  Created by Dinar Mukhametshin on 29.04.2020.
+//  Copyright © 2020 Dinar Mukhametshin. All rights reserved.
+//
+
+import Foundation
